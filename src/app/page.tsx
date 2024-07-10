c345e6e5380bee360f6d61ad1280711ebabf429c
kumait mustafa <kumait@gmail.com>
2024-07-10 14:55:54 +0200

    feat: Add Head component for displaying a header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import List from "../components/List";
 import data from "../data.json";
+import Head from "../components/Head";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl flex items-center justify-center font-bold text-3xl">
+        <Head />
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <List listObj={data} />
---
+++ src/components/Head.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Head = () => {
+    return (
+        <header>
+            <h1>Headline</h1>
+        </header>
+    );
+};
+export default Head;
