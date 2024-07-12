1216c77a7b3018f45957e71079dfe162c8f57ff7
kumait mustafa <kumait@gmail.com>
2024-07-12 21:59:07 +0200

    refactor: Update List and ListItem components with StyleX styling.

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,18 +1,64 @@
+import stylex from "@stylexjs/stylex";
+
 interface ListItemProps {
-    id: string
+    id: string;
     name: string;
     url: string;
 }
 
 const ListItem: React.FC<ListItemProps> = ({ id, name, url }) => (
-    <li key={id} className="flex items-center justify-between p-4 border rounded shadow-sm mb-2">
-        <span className="inline-block transition-transform group-hover:translate-x-1 text-lg font-medium motion-reduce:transform-none">{name}</span>
+    <li {...stylex.props(stylesListItem.listItem)} key={id}>
+        <span {...stylex.props(stylesListItem.itemName)}>{name}</span>
         <a
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            href={url} target="_blank" rel="noopener noreferrer" >
+            {...stylex.props(stylesListItem.itemLink)}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
             URL: {name}
         </a>
-    </li >
-
+    </li>
 );
+
 export default ListItem;
+
+const stylesListItem = stylex.create({
+    listItem: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '1rem',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderRadius: '0.25rem',
+        boxShadow: '0 1px 2px rgba(0, 0, 0, 0.1)',
+        marginBottom: '0.5rem',
+    },
+    itemName: {
+        display: 'inline-block',
+        transition: 'transform 0.2s',
+        fontSize: '1.125rem',
+        fontWeight: 500,
+        transform: {
+            ':hover': 'translateX(0.25rem)',
+        },
+    },
+    itemLink: {
+        display: 'block',
+        borderRadius: '0.5rem',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        padding: '1.25rem',
+        transition: 'border-color 0.2s, background-color 0.2s',
+
+        borderColor: {
+            default: 'transparent',
+            '@media (prefers-color-scheme: dark)': '#3154e0ae',
+            ':hover': '#d1d5db',
+        },
+        backgroundColor: {
+            ':hover': '#f3f3f6da',
+
+        },
+    },
+});
