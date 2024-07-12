b91a08888427201169089d6e7e2cd475cbd9da14
kumait mustafa <kumait@gmail.com>
2024-07-12 16:51:28 +0200

    Styling

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -6,8 +6,10 @@ interface ListItemProps {
 
 const ListItem: React.FC<ListItemProps> = ({ id, name, url }) => (
     <li key={id} className="flex items-center justify-between p-4 border rounded shadow-sm mb-2">
-        <span className="text-lg font-medium">{name}</span>
-        <a href={url} target="_blank" rel="noopener noreferrer" >
+        <span className="inline-block transition-transform group-hover:translate-x-1 text-lg font-medium motion-reduce:transform-none">{name}</span>
+        <a
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            href={url} target="_blank" rel="noopener noreferrer" >
             URL: {name}
         </a>
     </li >
